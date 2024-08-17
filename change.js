f638c85cfac676366dc48990ee467d632595c4f3
Baivab Bhusal <118292207+baivabbhusal@users.noreply.github.com>
2024-08-17 16:29:15 +0545

    Create javaScript files

---
+++ change.js
@@ -0,0 +1,34 @@
+const body=document.querySelector("body");
+const buttons=document.querySelectorAll(".button");
+
+buttons.forEach(
+function(button){
+    button.addEventListener("click",function(e){
+
+            if(e.target.id=="red")
+            {
+                body.style.backgroundColor=e.target.id;
+                
+            }
+            if(e.target.id=="yellow")
+                {
+                    body.style.backgroundColor=e.target.id;
+                }
+                    
+            if(e.target.id=="green")
+            {
+                body.style.backgroundColor=e.target.id;
+              
+            }
+            if(e.target.id=="blue")
+                {
+                    body.style.backgroundColor=e.target.id;   
+                }
+                
+            }
+        );
+    
+}
+);
+
+
